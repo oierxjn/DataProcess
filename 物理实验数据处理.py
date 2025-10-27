e2c0cdeda5a6e23cc102c8d17a76c7ec7eaa67c0
oierxjn <1934672944@qq.com>
2025-10-27 13:54:52 +0800

    添加配置文件支持并优化数据处理逻辑

--- 物理实验数据处理.py
+++ 物理实验数据处理.py
@@ -1,10 +1,29 @@
 import data_process
 
+position = None
+unit_list = None
+unit = []
+
+with open("config.txt", "r") as f:
+    lines = f.readlines()
+    position = lines[0].strip().split()
+    unit_list = lines[1].strip().split()
+    for i in unit_list:
+        if i == "mm":
+            unit.append(0.1)
+        else:
+            unit.append(1)
+
+
 origin_data = data_process.GetData()
 
 origin_data.file_get("1.txt").lines_extract_data()
 
-data = data_process.GetData(origin_data.extract_data_from_data(func=lambda x: [x[0], x[1], x[2]]))
+data = data_process.GetData(
+    origin_data.extract_data_from_data(
+        func=lambda x: [x[int(position[0])]*unit[int(position[0])], x[int(position[1])]*unit[int(position[1])], x[int(position[2])]*unit[int(position[2])]]
+        )
+    )
 
 data.data_add_byfunc(lambda x: (x[1] - x[2])) #3
 data.data_add_byfunc(lambda x: (x[3]*x[3])) #4
@@ -35,6 +54,6 @@ for i in data.data:
 data.data_add_byfunc(add_data) #5
 data.data_add_byfunc(lambda x: x[5]/(20*(589.3*0.0000001))) #6
 
-col = ['级次', 'x1', 'x2', 'D_1', 'D_1^2', 'D_m-D_n', 'R_{m-n}']
+col = ['级次', '$x1$', '$x2$', '$D_1$', '$D_1^2$', '$D_m-D_n$', '$R_{m-n}$']
 
 data.table_drawing(columns=col ,title="用牛顿环测透镜的曲率半径实验数据记录表")
