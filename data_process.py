d3dbfd1083c0f2a1c479cf9c0516668116f3bf83
oierxjn <1934672944@qq.com>
2025-10-27 13:17:05 +0800

    修复LaTeX转义字符并添加文件读取注释

--- data_process.py
+++ data_process.py
@@ -54,8 +54,8 @@ class UnivariateLinearRegression(slm.LinearRegression):
 
         plt.scatter(x, y, color="blue", label=title)
         plt.plot(x, self.predict(x), color="red", linewidth=2, label="拟合直线")
-        plt.xlabel(f"${x_label} \; {x_unit}$")
-        plt.ylabel(f"${y_label} \; {y_unit}$")
+        plt.xlabel(f"${x_label} \\; {x_unit}$")
+        plt.ylabel(f"${y_label} \\; {y_unit}$")
         plt.title(f"${y_label}-{x_label}$ {title_description}")
 
         # mid_x = (x.min() + x.max()) / 2
@@ -75,6 +75,9 @@ class GetData:
         return
     
     def file_get(self, file_name="1.txt"):
+        '''
+        在文件中提取数据到lines属性
+        '''
         lines: list = None
         with open(file_name,'r') as f:
             lines = f.readlines()
@@ -226,7 +229,7 @@ class GetData:
         将数据列表的每一行传入给定函数，并将给定函数的返回值作为一行，返回新数据列表。
 
         参数：
-            func：传入的函数。将传入数据的一行为参数，需要有返回值。
+            func：传入的函数。将传入数据的一行为参数，返回值为新行。
             data：要处理的数据列表，默认使用self.data。
         返回：
             out_data：处理后的新数据列表。
